85223eb99740b160b01239a5a5e86f7414c1b5ea
Du4ert <du4ertb@gmail.com>
2020-08-07 15:46:47 +0300

    Friday save

--- src/js/main.js
+++ src/js/main.js
@@ -1,46 +1,18 @@
-const more = document.querySelectorAll('.content-more');
 const resources = document.querySelectorAll('.resource');
 
-
-resources.forEach((item) => {
-    const imgUrl = item.querySelector('.resource-img').src;
+resources.forEach(item => {
     const cover = item.querySelector('.resource-cover');
-    
-    //cover.style.backgroundImage = `url(${imgUrl})`;
-    cover.addEventListener('click', function() {
-        item.classList.add('hide');
-        item.classList.add('show');
+    const title = item.querySelector('.resource-title').textContent;
 
-        document.body.addEventListener('click', function(e) {
-            const target = e.target;
-
-            if (target.closest('.resource') !== item) {
-                cover.classList.remove('hide');
-                item.classList.remove('show');
-            }
-        });
-    });
-})
-
-function toggleShow(elem) {
-    elem.classList.toggle('show');
-}
-
-function toggleArrow(elem) {
-    elem.classList.toggle('fa-arrow-up');
-    elem.classList.toggle('fa-arrow-down');
-}
-
-
-more.forEach(item => {
-    item.addEventListener('click', function(event) {
-        const parent = this;
+    cover.textContent = title;
+    item.addEventListener('click', (event) => {
         const target = event.target;
-        const content = parent.querySelector('.more-body')
-        const arrow = parent.querySelector('.more-trigger .fa')
-        if (target.closest('.more-trigger')) {
-            toggleShow(content);
-            toggleArrow(arrow);
-        }
+        item.classList.toggle('active');
+
+        document.body.addEventListener('click', (event) => {
+            if (event.target.closest('.resource') !== item) {
+                item.classList.remove('active');
+            }
+        })
     })
-})
+});
